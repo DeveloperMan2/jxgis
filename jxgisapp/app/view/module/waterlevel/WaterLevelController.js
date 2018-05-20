c50031d9565b9a310e7bb3028f0e393e60fe2f05
winnerlbm <winnerlbm@163.com>
2018-05-20 17:11:28 +0800

    添加echart脚本库

--- jxgisapp/app/view/module/waterlevel/WaterLevelController.js
+++ jxgisapp/app/view/module/waterlevel/WaterLevelController.js
@@ -1,6 +1,9 @@
 /**
  * Created by winnerlbm on 2018/5/19.
  */
+
+//用于测试的数据服务
+//http://localhost:8090/ajs/doc/sdk/sample-code/layers-featurelayer-collection/live/data/week.geojson
 Ext.define('jxgisapp.view.module.waterlevel.WaterLevelController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.waterlevel',
