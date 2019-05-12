d196d119c520b44ea20e992d5aff926d31037edf
564940585@qq.com <564940585@qq.com>
2019-05-13 07:39:41 +0800

    合并

---
+++ jxgisapp/app/view/module/waterpump/WaterPumpModel.js
@@ -0,0 +1,23 @@
+/**
+ * Created by winnerlbm on 2018/5/19.
+ */
+Ext.define('jxgisapp.view.module.waterpump.WaterPumpModel', {
+        extend: 'Ext.app.ViewModel',
+        alias: 'viewmodel.waterpump',
+
+        stores: {
+            /*
+            A declaration of Ext.data.Store configurations that are first processed as binds to produce an effective
+            store configuration. For example:
+
+            users: {
+                model: 'WaterPump',
+                autoLoad: true
+            }
+            */
+        },
+
+        data: [
+        ]
+    }
+);
